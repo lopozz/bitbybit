074478b7a2af8ad98603d2e0bc3a2adcea8e6def
Lorenzo Pozzi <lpozzi@pcopernico.it>
2025-11-30 19:54:14 +0100

    linter

--- bitbybit/autoencoders/ae.py
+++ bitbybit/autoencoders/ae.py
@@ -1,8 +1,8 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Encoder(nn.Module):
 
+class Encoder(nn.Module):
     def __init__(self, in_channels=1, hidden_dim=256, n_layers=6, verbose=False):
         super().__init__()
         self.verbose = verbose
@@ -23,21 +23,21 @@ class Encoder(nn.Module):
         # x: [B, C, T]
         if self.verbose:
             print(f"Input: {x.shape}")
-        print('Encoder')
+            print("Encoder")
         for i, conv in enumerate(self.convs):
             x = conv(x)
             x = F.relu(x)
 
             if self.verbose:
-                print(f"After layer {i+1}: {x.shape}")
+                print(f"After layer {i + 1}: {x.shape}")
 
         return x
 
 
 class Decoder(nn.Module):
-
-    def __init__(self, out_channels=1, hidden_dim=256, n_layers=6):
+    def __init__(self, out_channels=1, hidden_dim=256, n_layers=6, verbose=False):
         super().__init__()
+        self.verbose = verbose
         deconvs = []
         for i in range(n_layers):
             deconvs.append(
@@ -53,13 +53,14 @@ class Decoder(nn.Module):
         self.deconvs = nn.ModuleList(deconvs)
 
     def forward(self, x):
-        print('Decoder')
+        if self.verbose:
+            print("Decoder")
         for i, deconv in enumerate(self.deconvs):
             x = deconv(x)
             if i < len(self.deconvs) - 1:
                 x = F.relu(x)
                 if self.verbose:
-                    print(f"After layer {i+1}: {x.shape}")
+                    print(f"After layer {i + 1}: {x.shape}")
         if self.verbose:
             print(f"Output: {x.shape}")
         return x
@@ -69,18 +70,11 @@ class AE(nn.Module):
     def __init__(self, hidden_dim=512, n_layers=6, verbose=False):
         super().__init__()
         self.enc = Encoder(
-            in_channels=1,
-            hidden_dim=hidden_dim,
-            n_layers=n_layers,
-            verbose=verbose
+            in_channels=1, hidden_dim=hidden_dim, n_layers=n_layers, verbose=verbose
         )
-        self.dec = Decoder(
-            out_channels=1,
-            hidden_dim=hidden_dim,
-            n_layers=n_layers
-        )
+        self.dec = Decoder(out_channels=1, hidden_dim=hidden_dim, n_layers=n_layers)
 
     def forward(self, x):
-        z = self.enc(x) # [B, C, T_e]
-        y = self.dec(z) # [B, C, T]
+        z = self.enc(x)  # [B, C, T_e]
+        y = self.dec(z)  # [B, C, T]
         return y
